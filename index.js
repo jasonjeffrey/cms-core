18029ca9ffba83e869394aebcfefbdafe25d02af
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2015-06-30 15:39:11 +0100

    tiy up port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.startServer = function () {
   var app = require('express')();
 
-  app.listen(app.get('port'), function () {
+  app.listen(8080, function () {
     console.log('Server listening at http://%s:%s', server.address().address, server.address().port);
   });
 };
