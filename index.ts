2d7b6cbf29b85e7060c0c6522ccf10a5fe513eb2
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2015-07-15 09:23:41 +0100

    update gitignore and tidy index.ts

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@ import Database = require('./libs/Database');
 import Server = require('./libs/Server');
 
 class Core {
-    public static Database = Database; 
+    public static Database = Database;
     public static Server = Server;
-    
-    public start():void {
-        var server =  new Server();
-        
+
+    public start(): void {
+        var server = new Server();
+
         server.start();
     }
 }
